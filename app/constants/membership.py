faa435f944a667f3ce95d6022ff416a32febdf3b
Emiliano Jankowski <ejankowski@gmail.com>
2025-08-03 00:40:30 +0200

    Exposing membership types endpoint

--- app/constants/membership.py
+++ app/constants/membership.py
@@ -1,8 +1,15 @@
 """Constants for membership roles."""
 
 OWNER_ROLE = "owner"
-ADMIN_ROLE = "admin"
+ADMIN_ROLE = "administrator"
 MEMBER_ROLE = "member"
 
 # List of all valid roles
 VALID_ROLES = [OWNER_ROLE, ADMIN_ROLE, MEMBER_ROLE]
+
+# Role data for API responses
+ROLES_DATA = [
+    {"id": OWNER_ROLE, "name": "Owner"},
+    {"id": ADMIN_ROLE, "name": "Administrator"},
+    {"id": MEMBER_ROLE, "name": "Member"},
+]
